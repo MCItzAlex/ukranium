d92cd34d093c9189a18747d95cc300a19050ddd6
MCItzAlex <alex.balsillie@gmail.com>
2022-08-08 02:40:31 +0100

    added raw and ingot forms of ukranium

--- src/main/java/com/alex2k/ukranium/Ukranium.java
+++ src/main/java/com/alex2k/ukranium/Ukranium.java
@@ -1,8 +1,10 @@
 package com.alex2k.ukranium;
 
+import com.alex2k.ukranium.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -19,7 +21,11 @@ public class Ukranium
     public Ukranium()
     {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/com/alex2k/ukranium/item/ModItems.java
+++ src/main/java/com/alex2k/ukranium/item/ModItems.java
@@ -1,13 +1,26 @@
 package com.alex2k.ukranium.item;
 
 import com.alex2k.ukranium.Ukranium;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.lwjgl.system.CallbackI;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Ukranium.MOD_ID);
 
+    public static final RegistryObject<Item> UKRANIUM_INGOT = ITEMS.register("ukranium_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    public static final RegistryObject<Item> RAW_UKRANIUM = ITEMS.register("raw_ukranium",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 
 }
