7867d2eab7afd19ee8b54d07d22862612e984746
MCItzAlex <alex.balsillie@gmail.com>
2022-08-08 02:23:40 +0100

    init

---
+++ src/main/java/com/alex2k/ukranium/item/ModItems.java
@@ -0,0 +1,13 @@
+package com.alex2k.ukranium.item;
+
+import com.alex2k.ukranium.Ukranium;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Ukranium.MOD_ID);
+
+
+}
